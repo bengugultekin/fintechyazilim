4f5174984c965b5a73552face42367dceb05c106
bengugultekin <bengunurgultekin54@gmail.com>
2021-06-27 15:39:18 +0300

    renewed component structure

---
+++ src/components/BootstrapDatatable.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app" class="container mt-3 mb-5">
+        <h2>Country Table</h2>
+        <b-row class="mb-3">
+            <b-col md="3">
+                <b-from-input></b-from-input>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col>
+                <b-table
+                striped
+                hover
+                outlined
+                :items="items"
+                :fields="fields"
+                :filter="filetr"
+                :current-page="currentPage"
+                :per-page="perPage"
+                :filter-included-fields="['name', 'capital', 'region']"
+                >
+
+                </b-table>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
