b4764665a22167ed33cddd45f04dfb08463ad355
bengugultekin <bengunurgultekin54@gmail.com>
2021-06-27 16:43:25 +0300

    added search bar and search filters

--- src/components/BootstrapDatatable.vue
+++ src/components/BootstrapDatatable.vue
@@ -3,7 +3,7 @@
         <h2>Country Table</h2>
         <b-row class="mb-3">
             <b-col md="3">
-                <b-from-input></b-from-input>
+                <b-form-input v-model="filter" type="search" id="filterInput" placeholder="Type to Search"></b-form-input>
             </b-col>
         </b-row>
 
@@ -15,18 +15,27 @@
                 outlined
                 :items="items"
                 :fields="fields"
-                :filter="filetr"
+                :filter="filter"
                 :current-page="currentPage"
                 :per-page="perPage"
                 :filter-included-fields="['name', 'capital', 'region']"
                 >
+
                 <template #cell(index)="data">
                     {{data.index + 1 }}
                 </template>
+
                 <template #cell(imgflag)="data">
                     <img :src="data.item.flag" width="40" height="30"/>
                 </template>
                 </b-table>
+
+                <b-pagination
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="perPage"
+                aria-controls="my-table"
+                ></b-pagination>
             </b-col>
         </b-row>
     </div>
@@ -42,8 +51,8 @@ import axios from 'axios';
                 items:[],
                 perPage: 10,
                 currentPage: 1,
-                fields: ['index', 'name', 'capital', 'region', {key:'imgflag', label:'Flag'}],
-                rows: 0
+                fields: ['index', 'name', 'capital', 'region', {key:'imgflag', label:'Flag'} ],
+                rows:0
             }
         },
         // Fetches posts when the components is created.
@@ -51,18 +60,15 @@ import axios from 'axios';
             axios.get(`https://restcountries.eu/rest/v2/all`)
             .then(
                 response => {
-
-                    this.items = response.data;
-                    this.fields = ['index', 'name', 'capital', 'region', {key:'imgflag', label:'Flag'}];
-                    this.rows = response.data.lenght
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                })
+                // JSON responses are automatically parsed.
+                this.items = response.data;
+                this.fields = ['index', 'name', 'capital', 'region', {key:'imgflag', label:'Flag'} ];
+                this.rows = response.data.length
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
         }
     }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
-import Vue from "vue";
-import "./plugins/bootstrap-vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false;
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
-  router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
